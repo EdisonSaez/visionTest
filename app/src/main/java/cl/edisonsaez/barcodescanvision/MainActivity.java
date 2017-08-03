9a93f3450dabd35e7716dc5196fb4537c6305d27
Edison Saez <esaez@autentia.cl>
2017-08-03 10:13:16 -0400

    first commit

---
+++ app/src/main/java/cl/edisonsaez/barcodescanvision/MainActivity.java
@@ -0,0 +1,77 @@
+package cl.edisonsaez.barcodescanvision;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.edwardvanraak.materialbarcodescanner.MaterialBarcodeScanner;
+import com.edwardvanraak.materialbarcodescanner.MaterialBarcodeScannerBuilder;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String BARCODE_KEY = "BARCODE";
+
+    private Barcode barcodeResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bt_scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startScan();
+            }
+        });
+    }
+
+    private void startScan() {
+
+        final MaterialBarcodeScanner materialBarcodeScanner = new MaterialBarcodeScannerBuilder()
+                .withActivity(MainActivity.this)
+                .withEnableAutoFocus(true)
+                .withBleepEnabled(true)
+                .withBackfacingCamera()
+                .withText("Scanning...")
+                .withResultListener(new MaterialBarcodeScanner.OnResultListener() {
+                    @Override
+                    public void onResult(Barcode barcode) {
+                        barcodeResult = barcode;
+                        ((TextView) findViewById(R.id.res_txt)).setText(barcode.rawValue);
+                    }
+                })
+                .build();
+        materialBarcodeScanner.startScan();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode != MaterialBarcodeScanner.RC_HANDLE_CAMERA_PERM) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startScan();
+            return;
+        }
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(android.R.string.ok, listener)
+                .show();
+    }
+}
